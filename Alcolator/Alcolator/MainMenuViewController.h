b901cc99ff92ec929529e169310de9cd105c2c65
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-03-29 13:22:42 -0400

    Checkpoint 21, navigation iitem troubles

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Joanna Lingenfelter on 3/29/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
--- Alcolator/Alcolator/WhiskeyViewController.m
+++ Alcolator/Alcolator/WhiskeyViewController.m
@@ -45,7 +45,7 @@
         whiskeyText = NSLocalizedString(@"shots", @"plural of shot");
     }
     NSString *resultText = [NSString stringWithFormat:NSLocalizedString(@"%d %@ (with %.2f%% alcohol) contains as much alcohol as %.1f %@ of whiskey.", nil), numberOfBeers, beerText, [self.beerPercentTextField.text floatValue], numberOfWhiskeyGlassesForEquivalentAlcoholAmount, whiskeyText];
-    self.resultLabel = resultText;
+    self.resultLabel.text = resultText;
 }
 
 @end
